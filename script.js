552005707d81cf575b5c96a48ae812ec679ea02c
raphael-pinheiro1 <raphaelteixeiraa1@gmail.com>
2022-05-03 06:35:20 -0300

    Firts commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/* function primeiraFaseJake () {
+    while (opcao != 1 || opcao != 2) {
+        var opcao = prompt (
+            `Escolha uma das opções:\n1. \n2. `);
+
+
+        if (opcao == 1) {
+            window.location.href = "../";
+            break;
+
+        } else if (opcao == 2) {
+            window.location.href = "../";
+            break;
+
+        } else {
+            alert (``);
+        }
+    }
+} */
