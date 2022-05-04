3267fc15e33ad0dfa6ef106e3a4b9542323519cc
raphael-pinheiro1 <raphaelteixeiraa1@gmail.com>
2022-05-04 05:54:41 -0300

    Termineide criar as fases do Dick Vigarista e terminei de elaboras as perguntas

--- dick/scriptJs/dickStageThree.js
+++ dick/scriptJs/dickStageThree.js
@@ -1,15 +1,15 @@
 function phaseOneDick () {
     while (opcao != 1 || opcao != 2) {
         var opcao = prompt (
-            `Escolha uma das opções:\n1. Ele ganhou todas as corridas.\n2. nunca ganhou uma corrida. `);
+            `Escolha uma das opções:\n1. Poderia enxergar o que ele estava errando e se aperfeiçoar\n2. Ser orgulho e continuar com seu sua maneira de ser desleal. `);
 
 
         if (opcao == 1) {
-            window.location.href = "../";
+            window.location.href = "dick/winDick.html";
             break;
 
         } else if (opcao == 2) {
-            window.location.href = "../";
+            window.location.href = "/gameOver_Dick.html";
             break;
 
         } else {
